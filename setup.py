f2245d2c5b49a39241c09876c2746f8c07a05066
Jakub Priban <jp775@cam.ac.uk>
2019-11-19 21:13:08 +0000

    folder structure

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name = 'Name-WIP',
+    version = '0.1',
+    description = 'Automatic generation of commutative diagrams',
+    url = 'https://github.com/pribanacek/Part-II-Project',
+    author = 'Jakub Priban',
+    license = 'something',
+    packages = [
+        'networkx'
+    ],
+    install_requires = [
+        'networkx',
+    ]
+)
