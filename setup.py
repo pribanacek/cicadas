ea6609f484712b054f3bcd30b4b62d6d0638dca0
Jakub Priban <jp775@cam.ac.uk>
2020-03-06 21:43:35 +0000

    pypi setup

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'CURTIS',
-    version = '0.1',
-    description = 'Automatic generation of commutative diagrams',
-    url = 'https://github.com/pribanacek/Part-II-Project',
-    author = 'Jakub Priban',
-    # license = 'something',
-    packages = find_packages('antlr', 'networkx', 'numpy')
+    name="cicadas",
+    version="1.0.0a1",
+    author="Jakub Priban",
+    author_email="jp775@cam.ac.uk",
+    description="Self-constructing commutative diagrams",
+    long_description_content_type="text/markdown",
+    url="https://github.com/pribanacek/cicadas",
+    packages=find_packages(where = 'src'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    project_urls={
+        'Bug Reports': 'https://github.com/pribanacek/cicadas/issues',
+        'Source': 'https://github.com/pribanacek/cicadas',
+    },
 )
