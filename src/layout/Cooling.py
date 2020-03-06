8d9773131bc7c9c632c65557fcd619c6103156cf
Jakub Priban <jp775@cam.ac.uk>
2020-03-06 12:54:05 +0000

    modify cooling

--- src/layout/Cooling.py
+++ src/layout/Cooling.py
@@ -1,44 +1,40 @@
 from abc import ABC, abstractmethod
+import math
 
 class Cooling(ABC):
-    def __init__(self, start, end, factor = None, steps = 100, resets = 0):
+    def __init__(self, start, end = 0.1, steps = 100, resets = 0):
         self.start_temp = start
         self.end_temp = end
-        self.temp = start
-        self.runs = 0
+        self.total_steps = steps
         self.resets = resets
-        if factor != None:
-            self.factor = factor
-        else:
-            self.factor = self._find_factor(steps)
+        self.step = 0
+        self.runs = 0
 
     def done(self):
-        return self.temp < self.end_temp
+        return self.get_temp() <= self.end_temp
 
     def cool(self):
-        self._decrease_temp()
+        self.step += 1
         if self.done() and self.runs < self.resets:
             self.runs += 1
-            self.temp = self.start_temp
+            self.step = 0
+    
+    def progress(self):
+        return self.step / self.total_steps
 
     @abstractmethod
-    def _decrease_temp(self):
-        pass
-    
-    @abstractmethod
-    def _find_factor(self, steps):
+    def get_temp(self):
         pass
 
 class LinearCooling(Cooling):
-    def _find_factor(self, steps):
-        return (self.start_temp - self.end_temp) / steps
-
-    def _decrease_temp(self):
-        self.temp -= self.factor
+    def get_temp(self):
+        x = self.progress()
+        t = (1 - x) * self.start_temp + x * self.end_temp
+        return t
 
 class ExpCooling(Cooling):
-    def _find_factor(self, steps):
-        return (self.end_temp / self.start_temp) ** (1 / steps)
-
-    def _decrease_temp(self):
-        self.temp *= self.factor
+    def get_temp(self):
+        x = self.progress()
+        k = math.log(self.start_temp / self.end_temp)
+        t = self.start_temp * math.exp(-k * x)
+        return t
