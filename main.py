349efb37ca4c82aa7bd5b774917a9ca955a57b53
Jakub Priban <jp775@cam.ac.uk>
2020-03-06 21:43:06 +0000

    fix some cli args

--- main.py
+++ main.py
@@ -19,37 +19,38 @@ import src.tests.TikzGeneration
 import cProfile
 
 
-def main(filename, output_path = './output'):
-    graph_assembler = Parser.parse(filename)
+def main(input_file, output_path, n, preview, clipboard):
+    graph_assembler = Parser.parse(input_file)
     GraphMeasure.measure(graph_assembler.nodes, graph_assembler.edges, graph_assembler.regions)
     graph = graph_assembler.getGraph()
-    (min_graph, graphs) = optimize_layout(graph)
+    (min_graphs, graphs) = optimize_layout(graph, n)
 
     if os.environ['CICADAS_VIZ'] == 'True':
         import src.visualisation.animate as animate
         animate.start_animation_thread(graphs)
 
-    result = generate_tikz(min_graph)
-
+    result = generate_tikz(min_graphs)
     generatePDF(result, output_path)
-    full_path = os.path.abspath(output_path)
-    webbrowser.open_new('file://' + full_path + '.pdf')
-    # os.remove(fullPath + '.pdf')
+
+    if preview:
+        full_path = os.path.abspath(output_path)
+        webbrowser.open_new('file://' + full_path + '.pdf')
+    
+    print('Successfully generated diagram:\n')
     print(result)
 
 
 if __name__ == "__main__":
     args = arg_parser.parse_args(sys.argv[1:]) # TODO check this off-by-one
-    # print(vars(args))
 
     # cProfile.run('main(filename)')
-    if args.silent:
-        Logging.silent = True
+    # if args.silent:
+    #     Logging.silent = True
     if args.suppress_warnings:
         Logging.suppress_warnings = True
 
     try:
-        main(args.filename)
+        main(args.filename, args.o, args.n, args.preview, args.auto_clipboard)
     except FileNotFoundError as e:
         print(e)
     except ParsingException as e:
@@ -61,5 +62,8 @@ if __name__ == "__main__":
         print('Error occurred when compiling LaTeX:', e.msg)
         print('pdflatex subprocess returned the following error:')
         print(e.error)
+    except PermissionError as e:
+        print('Permission error occurred')
+        print('Make sure you have permission to read the source, and to read and write to the destination')
     except Exception as e:
         print(e)
--- src/ui/ArgParser.py
+++ src/ui/ArgParser.py
@@ -5,33 +5,46 @@ DESCRIPTION = 'CICADAS description.'
 arg_parser = argparse.ArgumentParser(description = 'CICADAS description')
 
 arg_parser.add_argument('filename',
-    help = 'the input file to be parsed'
+    help = 'the input file to be parsed',
+)
+
+arg_parser.add_argument('-o',
+    help = 'output destination (will generate a .pdf and a .tex file of that name). Default: \'./output\'',
+    action = 'store',
+    required = False,
+    default = './output'
 )
 
 arg_parser.add_argument('-n',
     help = 'number of candidate diagrams to generate',
-    action = 'store', required = False
+    action = 'store',
+    type = int,
+    required = False,
+    default = 1
 )
 
-arg_parser.add_argument('-s', '--strategy',
-    help = 'layout strategy to use',
-    action = 'store', required = False
-)
+# arg_parser.add_argument('-s', '--strategy',
+#     help = 'layout strategy to use',
+#     action = 'store',
+#     type = int,
+#     required = False,
+#     default = 0
+# )
 
-arg_parser.add_argument('-d', '--auto-display-pdf',
-    help = 'automatically copy output to clipboard',
+arg_parser.add_argument('-p', '--preview',
+    help = 'automatically open generated pdf when finished',
     action = 'store_true', required = False
 )
 
-arg_parser.add_argument('-ac', '--auto-clipboard',
-    help = 'automatically copy output to clipboard',
+arg_parser.add_argument('-c', '--auto-clipboard',
+    help = 'automatically copy tikz output to clipboard',
     action = 'store_true', required = False
 )
 
-arg_parser.add_argument('--silent',
-    help = 'suppress all messages',
-    action = 'store_true', required = False
-)
+# arg_parser.add_argument('--silent',
+#     help = 'suppress all messages',
+#     action = 'store_true', required = False
+# )
 
 arg_parser.add_argument('--suppress-warnings',
     help='suppresses warnings',
